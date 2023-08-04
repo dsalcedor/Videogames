3721a73f4afa1e4f5d0192f500304b42a8876f8a
DARIO SALCEDO <DARIO_SALCEDO@jabil.com>
2023-08-04 05:23:55 -0600

    validacion de componentes

--- PI-Videogames-main/client/src/components/NavBar/NavBar.jsx
+++ PI-Videogames-main/client/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 function NavBar() {
   return (
     <div>
       <Link to="/home">Home</Link>
       <Link to="/form">Create</Link>
+      <SearchBar />
     </div>
   );
 }
--- PI-Videogames-main/client/src/views/Home/Home.jsx
+++ PI-Videogames-main/client/src/views/Home/Home.jsx
@@ -1,9 +1,20 @@
-import NavBar from "../../components/NavBar/NavBar";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 function Home() {
+
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    axios.get('localhost:3001/videogames')
+    .then(res=>res.json)
+    .then(data=> {
+      setGames([...games, data])
+    }) 
+  }, [])
+
   return (
     <div>
-      {/* <NavBar /> */}
       <h1>Esta es la vista Home</h1>
     </div>
   );
